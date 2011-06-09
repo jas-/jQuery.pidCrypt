9223d5c62d10f9c3673f4de2072a3cd131cacfb0
Jas- <jason.gerfen@gmail.com>
2011-06-09 14:00:37 -0600

    Added and fixed a couple of small bugs

--- proxy.php
+++ proxy.php
@@ -47,10 +47,10 @@ if (file_exists('libs/classes/class.openssl.php')) {
 
     /* Because we want to avoid MITM use AES to encrypt public key first */
     if ((!empty($_POST['u']))&&(!empty($_POST['i']))){
-     echo $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key']['key'];
+     echo $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key'];
      //echo $openssl->aesEnc($_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key'], $_POST['u'], $_POST['i'], false, 'aes-256-cbc');
     } else {
-     echo $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key']['key'];
+     echo $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key'];
     }
     exit;
    }
@@ -58,6 +58,10 @@ if (file_exists('libs/classes/class.openssl.php')) {
    if (!empty($_POST['do'])){
     switch($_POST['do']){
      case 'sign':
+      echo response(sign(array('name'=>$_POST['name'],'email'=>$_POST['email'],'message'=>$_POST['message']),
+                         $_SESSION[$_SERVER['REMOTE_ADDR'].'-private-key'],
+                         $_SESSION[$_SERVER['REMOTE_ADDR'].'-certificate'],
+                         $_SERVER['REMOTE_ADDR'], $openssl));
       exit;
      case 'sign_encrypt':
       exit;
@@ -68,42 +72,86 @@ if (file_exists('libs/classes/class.openssl.php')) {
 
    /*
     * If you wish to do anything further such as add a response that the data was recieved by the server etc
-    * add it here
+    * add it here (delete this because it returns the decrypted examples)
     */
-   echo 'Data recieved and processed...';
+   $response = 'Data recieved and processed...<br/>';
+   $response .= arr2json(helper(array('name'=>$_POST['name'],
+                                      'email'=>$_POST['email'],
+                                      'message'=>$_POST['message']),
+                                $openssl));
+   echo $response;
   }
  }
 }
 
 /*
- * Create private/public/certificate for referring machine
+ * Create private/public/certificate for referring machine (stored in session)
  */
 function create($settings, $openssl)
 {
  /* Generate the private key */
  $_SESSION[$_SERVER['REMOTE_ADDR'].'-private-key'] = $openssl->genPriv($_SERVER['REMOTE_ADDR']);
 
- /* Here we can either generate a public key or a certificate holding a public key */
- $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key'] = $openssl->genPub();
+ /* Get the public key */
+ $k = $openssl->genPub();
+ $_SESSION[$_SERVER['REMOTE_ADDR'].'-public-key'] = $k['key'];
 
  /* Create certificate */
- $_SESSION[$_SERVER['REMOTE_ADDR'].'-certificate'] = $openssl->newCert($settings['dn'],
-                                                                       $_SESSION[$_SERVER['REMOTE_ADDR'].'-private-key'],
-                                                                       $settings['config']);
+ $_SESSION[$_SERVER['REMOTE_ADDR'].'-certificate'] = $openssl->handleCertificate($settings,
+                                                                                 $_SESSION[$_SERVER['REMOTE_ADDR'].'-private-key'],
+                                                                                 $_SERVER['REMOTE_ADDR']);
 }
 
 /*
  * Loop over post data and attempt to sign it
  */
-function sign($data, $cert, $key, $pass, $opts, $handle)
+function sign($data, $key, $pass, $certificate, $openssl)
 {
- $a = array();
+ $fp = fopen('tmp/cert', 'w');
+ fwrite($fp, $_SESSION[$_SERVER['REMOTE_ADDR'].'-certificate']);
+ fclose($fp);
+ $boundary = md5(uniqid(time()));
+ $boddy = "MIME-Version: 1.0\n";
+ $boddy .= "Content-Type: multipart/mixed; boundary=\"" . $boundary. "\"\n";
+ $boddy .= "Content-Transfer-Encoding: quoted-printable\n\n";
+ $boddy .= "This is a multi-part message in MIME format.\n\n";
+ $boddy .= "--$boundary\n";
+ $boddy .= "Content-Type: text/plain; charset=\"iso-8859-1\"\n";
+ $boddy .= "Content-Transfer-Encoding: quoted-printable\n\n";
+ $boddy .= $_POST['message']."\n\n";
+ $boddy .= "--$boundary--\n";
+ $msg = 'msg.txt';
+ $signed = 'signed.txt';
+ $fp = fopen('tmp/'.$msg, "w");
+ fwrite($fp, $boddy);
+ fclose($fp);
+ if (openssl_pkcs7_sign($msg, 'tmp/'.$signed, 'tmp/cert',
+    array($_SESSION[$_SERVER['REMOTE_ADDR'].'-private-key'], $_SERVER['REMOTE_ADDR']),
+    array("To" => $_POST['email'],
+         "From: jQuery.pidCrypt <jason.gerfen@gmail.com>",
+         "Subject" => "A test"), PKCS7_DETACHED)) {
+    exec(ini_get('sendmail_path') . ' < ' . 'tmp/'.$signed);
+  }
+/*
+ $data = helper($data, $openssl); $a=array();
  if (count($data)>0){
-  foreach($data as $key => $value){
-   //$a[$key] = $handle->sign($value, $key, $pass, 365, $opts);
+  foreach($data as $k => $v){
+   $a[$k] = $openssl->ssign($v, $key, $pass);
   }
  }
  return $a;
+*/
+}
+
+/*
+ * handle encoding of responses
+ */
+function response($array){
+ if (!function_exists('json_encode')) {
+  return arr2json($array);
+ } else {
+  return json_encode($array);
+ }
 }
 
 /*
@@ -137,4 +185,24 @@ function combine($array) {
  }
  return $a;
 }
+
+/*
+ * Use these if json_encode not available
+ */
+function arr2json( $array )
+{
+ foreach( $array as $key => $value ) $json[] = $key . ':' . php2js( $value );
+ if( count( $json ) > 0 ) return '{' . implode( ',', $json ) . '}';
+ else return '';
+}
+
+function php2js( $value )
+{
+ if( is_array( $value ) ) return arr2json( $val );
+ if( is_string( $value ) ) return '"' . addslashes( $value ) . '"';
+ if( is_bool( $value ) ) return 'Boolean(' . (int) $value . ')';
+ if( is_null( $value ) ) return '""';
+ return $value;
+}
+
 ?>
