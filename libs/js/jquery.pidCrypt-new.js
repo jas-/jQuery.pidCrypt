41fc03301404d48182b63946856f916a9ba4b601
jas- <jason.gerfen@gmail.com>
2012-01-31 03:50:08 -0700

    Tue Jan 31 03:50:08 MST 2012

---
+++ libs/js/jquery.pidCrypt-new.js
@@ -0,0 +1,653 @@
+(function($){
+
+ $.fn.pidCrypt = function(method) {
+
+  var defaults = defaults || {
+   appID:        'jQuery.pidCrypt',
+   storage:      'local',
+   formID:       $(this),
+   type:         'json',
+   aes:          '',
+   keys:         {},
+   use:          '',
+   callback:     function(){},
+   preCallback:  function(){},
+   errCallback:  function(){}
+  };
+
+  var methods = methods || {
+
+   init: function(o){
+    var opts = $.extend({}, defaults, o);
+    opts.aes = _encrypt.__sAES();
+    opts.keys = _keys.__existing(opts);
+    _keys.__hK(opts);
+    opts.use = _keys.__sK(opts, opts.keys);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   },
+
+   sign: function(o){
+    var opts = $.extend({}, defaults, o);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   },
+
+   verify: function(o){
+    var opts = $.extend({}, defaults, o);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   },
+
+   encrypt_sign: function(o){
+    var opts = $.extend({}, defaults, o);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   },
+
+   decrypt_verify: function(o){
+    var opts = $.extend({}, defaults, o);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   },
+
+   authenticate: function(o){
+    var opts = $.extend({}, defaults, o);
+    $('#'+opts.formID.attr('id')).on('submit', function(e){
+      e.preventDefault();
+    });
+    return true;
+   }
+  };
+
+  /**
+   * @object main
+   * @abstract Handles primary functions
+   */
+  var _main = _main || {
+
+   /**
+    * @object _do
+    * @abstract Performs all AJAX requests. Sets additional CSRF and checksum
+    *           header fields which can be verified on server
+    */
+   __do: function(o, a){
+    var _data = (typeof a=='object') ? _strings.__serialize(a) : a;
+    $.ajax({
+     form: o.formID.attr('id'),
+     url: o.formID.attr('action'),
+     type: o.formID.attr('method'),
+     data: _data,
+     crossDomain: (o.type==='jsonp') ? true : false,
+     beforeSend: function(xhr){
+      xhr.setRequestHeader('X-Alt-Referer', o.appID);
+      if (_validation.__vStr(_data)){
+       xhr.setRequestHeader('Content-MD5', pidCryptUtil.encodeBase64(pidCrypt.MD5(_data)));
+      } else {
+       xhr.setRequestHeader('Content-MD5', pidCryptUtil.encodeBase64(pidCrypt.MD5(o.appID)));
+      }
+      ((o.preCallback)&&($.isFunction(o.preCallback))) ?
+        o.preCallback(xhr) : false;
+     },
+     success: function(x, status, xhr){
+      ((o.callback)&&($.isFunction(o.callback))) ?
+        o.callback.call(x) : console.log(x);
+     },
+     error: function(xhr, status, error){
+      ((o.errCallback)&&($.isFunction(o.errCallback))) ?
+        o.errCallback.call(xhr, status, error) : false;
+     }
+    });
+   },
+
+   /**
+    * @function __gF
+    * @abstract Performs object creation of non-null form elements
+    */
+   __gF: function(o){
+    
+   }
+  }
+
+  /**
+   * @object keys
+   * @abstract Object providing interface for key retrieval and storage
+   */
+  var _keys = _keys || {
+
+   /**
+    * @function __hK
+    * @abstract Handles retrieval, encryption and storage of key
+    */
+   __hK: function(o){
+    if (_validation.__szCk(o.keys)==0){
+     var x = o.callback;
+     var y = function(){
+      var z = JSON.parse(this);
+      var email = (z.email) ? z.email : o.appID;
+      var key = (z.key) ? z.key : false;
+      var p = _keys.__gUUID(null); var obj = {}; obj[p] = {};
+      obj[p]['email'] = o.aes.encryptText(email, p, {nBits:256, salt:_keys.__strIV(p)});
+      obj[p]['key'] = o.aes.encryptText(key, p, {nBits:256, salt:_keys.__strIV(p)});
+      _storage.__sI(o.storage, _keys.__id(), JSON.stringify(obj));
+     }
+     o.callback = y;
+     _main.__do(o, {'k': true});
+    }
+    return true;
+   },
+
+   /**
+    * @function __sK
+    * @abstract Attempts to find email address for user specific key retrieval
+    */
+   __sK: function(o, z){
+    var _r = false;
+    if (_validation.__szCk(z)>0){
+     $.each(z, function(a,b){
+      var _x = new RegExp('/[0-9a-z-_.]{2,45}\@[0-9a-z-_.]{2,45}\.[a-z]{2,4}/gi');
+      var _e = o.aes.decryptText(b['email'], a, {nBits:256, salt:_keys.__strIV(a)});
+      if (_x.test(_e)){
+       return o.aes.decryptText(b['key'], a, {nBits:256, salt:_keys.__strIV(a)});
+      } else {
+       _r = o.aes.decryptText(b['key'], a, {nBits:256, salt:_keys.__strIV(a)});
+      }
+     });
+    }
+    return _r;
+   },
+
+   /**
+    * @function existing
+    * @abstract Function used to return configured options
+    *           as JSON object
+    */
+   __existing: function(o){
+    return (_storage.__gI(o.storage, _keys.__id())) ?
+      JSON.parse(_storage.__gI(o.storage, _keys.__id())) : false;
+   },
+
+   /**
+    * @function __id
+    * @abstract Need an id to associate the public key and other
+    *           configuration options with a hostname or url
+    */
+   __id: function(){
+    return (_validation.__vStr(location.host)) ?
+     location.host :
+      (_validation.__vStr(location.hostname)) ?
+       location.hostname :
+        'localhost';
+   },
+
+   /**
+    * @function gUUID
+    * @abstract Generate a uuid (RFC-4122) string or optional hex
+    *           string of specified length
+    */
+   __gUUID: function(len){
+    var chars = '0123456789abcdef'.split('');
+    var uuid = [], rnd = Math.random, r;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+    for (var i = 0; i < 36; i++){
+     if (!uuid[i]){
+      r = 0 | rnd()*16;
+      uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
+     }
+    }
+    return (len!==null) ? uuid.join('').replace(/-/g, '').split('',len).join('') :
+                          uuid.join('');
+   },
+
+   /**
+    * @function strIV
+    * @abstract Generate IV from string
+    */
+   __strIV: function(s){
+    return (s) ?
+     encodeURI(s.replace(/-/gi, '').substring(16,Math.ceil(16*s.length)%s.length)) :
+     false;
+   }
+  }
+
+  /**
+   * @object encrypt
+   * @abstract Handles encryption functionality
+   */
+  var _encrypt = _encrypt || {
+
+   /**
+    * @function eO
+    * @abstract Calls certParser() on public key, intializes results with
+    *           external pidCrypt.RSA object, performs public key encryption
+    *           on object and returns results as object
+    */
+   __eO: function(o, obj){
+    var x = {}; var y = this.__certParser(uP(o));
+    this.__iP(o, y);
+    if (szCk(obj)>0){
+     $.each(obj, function(a, b){
+      if (typeof b==='object'){
+       x[a]={};
+       $.each(b, function(k, v){
+        x[a][k] = pidCrypt.RSA.prototype.encrypt(v);
+       });
+      } else {
+       x[a] = pidCrypt.RSA.prototype.encrypt(b);
+      }
+     });
+    } else {
+     x = 0;
+    }
+    return x;
+   },
+
+   /**
+    * @function sAES
+    * @abstract Returns pidCrypt.AES.CBC object for client AES storage
+    */
+   __sAES: function(){
+    return new pidCrypt.AES.CBC();
+   },
+
+   /**
+    * @function iP
+    * @abstract Returns external pidCrypt.RSA object once certParse()
+    *           generates necessary bytes from public key
+    */
+   __iP: function(o, pub){
+    if (pub.b64){
+     var x = pidCryptUtil.decodeBase64(pub.b64);
+     var rsa = new pidCrypt();
+     var asn = pidCrypt.ASN1.decode(pidCryptUtil.toByteArray(x));
+     var tree = asn.toHexTree();
+     pidCrypt.RSA.prototype.setPublicKeyFromASN(tree);
+    }
+    return rsa;
+   },
+
+   /*
+    * parse public/private key function
+    * (Copyright https://www.pidder.com/pidcrypt/?page=demo_rsa-encryption)
+    */
+   __certParser: function(cert){
+    var lines = cert.split('\n');
+    var read = false;
+    var b64 = false;
+    var end = false;
+    var flag = '';
+    var retObj = {};
+    retObj.info = '';
+    retObj.salt = '';
+    retObj.iv;
+    retObj.b64 = '';
+    retObj.aes = false;
+    retObj.mode = '';
+    retObj.bits = 0;
+    for(var i=0; i< lines.length; i++){
+     flag = lines[i].substr(0,9);
+     if(i==1 && flag != 'Proc-Type' && flag.indexOf('M') == 0)//unencrypted cert?
+     b64 = true;
+     switch(flag){
+      case '-----BEGI':
+       read = true;
+       break;
+      case 'Proc-Type':
+       if(read)
+        retObj.info = lines[i];
+        break;
+      case 'DEK-Info:':
+       if(read){
+        var tmp = lines[i].split(',');
+        var dek = tmp[0].split(': ');
+        var aes = dek[1].split('-');
+        retObj.aes = (aes[0] == 'AES')?true:false;
+        retObj.mode = aes[2];
+        retObj.bits = parseInt(aes[1]);
+        retObj.salt = tmp[1].substr(0,16);
+        retObj.iv = tmp[1];
+       }
+       break;
+      case '':
+       if(read)
+        b64 = true;
+        break;
+      case '-----END ':
+       if(read){
+        b64 = false;
+        read = false;
+       }
+       break;
+      default:
+       if(read && b64)
+        retObj.b64 += pidCryptUtil.stripLineFeeds(lines[i]);
+     }
+    }
+    return retObj;
+   }
+  }
+
+  /**
+   * @object strings
+   * @abstract Handles string processing
+   */
+  var _strings = _strings || {
+
+   /**
+    * @function _serialize
+    * @abstract Create serialized string of object
+    */
+   __serialize: function(args){
+    if (_validation.__szCk(args)>0){
+     var x='';
+     $.each(args, function(a, b){
+      if (typeof b==='object'){
+       this.__serialize(b);
+      } else {
+       x+=a+'='+b+'&';
+      }
+     });
+    } else {
+     return false;
+    }
+    return x;
+   },
+
+   /**
+    * @function sSplt
+    * @abstract Splits string length helper to overcome limitations with RSA
+    *           cipher and key sizes
+    */
+   __sSplt: function(str){
+    var t = str.length/80;
+    var y = {}; var x=0; var z=80;
+    for (var i=0; i<t; i++) {
+     if (i>0) { x=x+80; z=z+80; }
+     if (str.slice(x, z).length>0) {
+      y[i] = str.slice(x, z);
+     }
+    }
+    return y;
+   }
+  }
+
+  /**
+   * @object storage
+   * @abstract Object to provide interface to key storage options
+   */
+  var _storage = _storage || {
+
+   /**
+    * @function sI
+    * @abstract Proxy function for setting data with specified client storage
+    *           option
+    */
+   __sI: function(type, k, v){
+    var x = false;
+    type = (_validation.__vStore(type+'Storage')) ? type : 'cookie';
+    switch(type) {
+     case 'local':
+      x = this.__sL(k, v);
+      break;
+     case 'session':
+      x = this.__sS(k, v);
+      break;
+     case 'cookie':
+      x = this.__sC(k, v);
+      break;
+     default:
+      x = this.__sL(k, v);
+      break;
+    }
+    return x;
+   },
+
+   /**
+    * @function gI
+    * @abstract Proxy function for getting data with specified client storage
+    *           option
+    */
+   __gI: function(type, k){
+    var x = false;
+    type = (_validation.__vStore(type+'Storage')) ? type : 'cookie';
+    switch(type) {
+     case 'local':
+      x = this.__gL(k);
+      break;
+     case 'session':
+      x = this.__gS(k);
+      break;
+     case 'cookie':
+      x = this.__gC(k);
+      break;
+     default:
+      x = this.__gL(k);
+      break;
+    }
+    return x;
+   },
+
+   /**
+    * @function dI
+    * @abstract Proxy function for deleting data with specified client storage
+    *           option
+    */
+   __dI: function(type, k){
+    var x = false;
+    type = (_validation.__vStore(type+'Storage')) ? type : 'cookie';
+    switch(type) {
+     case 'local':
+      x = this.__dL(k);
+      break;
+     case 'session':
+      x = this.__dS(k);
+      break;
+     case 'cookie':
+      x = this.__dC(k);
+      break;
+     default:
+      x = this.__dL(k);
+      break;
+    }
+    return x;
+   },
+
+   /**
+    * @function sL
+    * @abstract Function used to set localStorage items
+    */
+   __sL: function(k, v){
+    return (localStorage.setItem(k, v)) ? false : true;
+   },
+
+   /**
+    * @function sS
+    * @abstract Function used to set sessionStorage items
+    */
+   __sS: function(k, v){
+    return (sessionStorage.setItem(k, v)) ? false : true;
+   },
+
+   /**
+    * @function sC
+    * @abstract Function used to set cookie items
+    */
+   __sC: function(k, v){
+    if (typeof $.cookie === 'function') {
+     return ($.cookie(k, v, {expires: 7})) ? true : false;
+    } else {
+     return false;
+    }
+   },
+
+   /**
+    * @function gL
+    * @abstract Function used to get localStorage items
+    */
+   __gL: function(k){
+    return (localStorage.getItem(k)) ? localStorage.getItem(k) : false;
+   },
+
+   /**
+    * @function sS
+    * @abstract Function used to get sessionStorage items
+    */
+   __gS: function(k){
+    return (sessionStorage.getItem(k)) ? sessionStorage.getItem(k) : false;
+   },
+
+   /**
+    * @function sC
+    * @abstract Function used to get cookie items
+    */
+   __gC: function(name){
+    if (typeof $.cookie === 'function') {
+     return ($.cookie(name)) ? $.cookie(name) : false;
+    } else {
+     return false;
+    }
+   },
+
+   /**
+    * @function dL
+    * @abstract Function used to delete localStorage items
+    */
+   __dL: function(k){
+    return (localStorage.removeItem(k)) ? localStorage.removeItem(k) : false;
+   },
+
+   /**
+    * @function dS
+    * @abstract Function used to delete sessionStorage items
+    */
+   __dS: function(k){
+    return (sessionStorage.removeItem(k)) ? sessionStorage.removeItem(k) : false;
+   },
+
+   /**
+    * @function dC
+    * @abstract Function used to delete cookie items
+    */
+   __dC: function(name){
+    if (typeof $.cookie === 'function') {
+     return ($.cookie(name, '', {expires: -7})) ? true : false;
+    } else {
+     return false;
+    }
+   }
+  }
+
+  /**
+   * @method validation
+   * @abstract Provides interface to validation functionality
+   */
+  var _validation = _validation || {
+
+   /**
+    * @function vStr
+    * @abstract Function used combine string checking functions
+    */
+   __vStr: function(x){
+    return ((x===false)||(x.length===0)||(!x)||(x===null)||
+            (x==='')||(typeof x==='undefined')) ? false : true;
+   },
+
+   /**
+    * @function vStore
+    * @abstract Function used to validate client storage option
+    */
+   __vStore: function(type){
+    try {
+     return ((type in window)&&(window[type])) ? true : false;
+    } catch (e) {
+     return false;
+    }
+   },
+   
+   /**
+    * @function szCk
+    * @abstract Performs a check on object sizes
+    */
+   __szCk: function(obj){
+    var n = 0;
+    $.each(obj, function(k, v){
+     if (obj.hasOwnProperty(k)) n++;
+    });
+    return n;
+   }
+  }
+
+  /**
+   * @function debugging method
+   * @abstract Handles any debugging functionality
+   */
+  var _debug = _debug || {
+
+   /**
+    * @function __recurse
+    * @abstract Function used help debug objects recursively
+    */
+   __recurse: function(obj){
+    $.each(obj, function(x,y){
+     if (typeof y==='object'){
+      console.log('Key: '+x);
+      _debug.__recurse(y);
+     } else {
+      console.log(x+' => '+y);
+     }
+    });
+   },
+
+   /**
+    * @function __dependencies
+    * @abstract Function used to log errors regarding necessary libraries
+    */
+   __dependencies: function(opts){
+    var ret = true;
+    if (!$.isFunction(pidCrypt.RSA.prototype.encrypt)){
+     console.log('pidCrypt RSA libraries are missing.'+
+                 'Please include the pidCrypt RSA libs...');
+     console.log('Download them from https://www.pidder.com/pidcrypt/');
+     console.log('See README document for necessary includes');
+     ret = false;
+    }
+    if (!$.isFunction(pidCrypt.AES.CBC)){
+     console.log('pidCrypt AES-CBC libraries are missing.'+
+                 'Please include the pidCrypt AES-CBC libs...');
+     console.log('Download them from https://www.pidder.com/pidcrypt/');
+     console.log('See README document for necessary includes');
+     ret = false;
+    }
+    if (opts.storage==='cookie'){
+     if (!$.isFunction($.cookie)){
+      console.log('Cookie use specified but required libraries not available.'+
+                  'Please include the jQuery cookie plugin...');
+      console.log('Download it from https://github.com/carhartl/jquery-cookie');
+      ret = false;
+     }
+    }
+    return ret;
+   }
+  }
+
+  /* robot, do something */
+  if (methods[method]){
+   return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+  } else if ((typeof method==='object')||(!method)){
+   return methods.init.apply(this, arguments);
+  } else {
+   console.log('Method '+method+' does not exist');
+  }
+ };
+})(jQuery);
